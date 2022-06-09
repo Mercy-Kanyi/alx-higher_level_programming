4a8f7fc74269be9afaf093c13c56840ad518a857
Mercy Kanyi <99387423+Mercy-Kanyi@users.noreply.github.com>
2022-06-09 07:48:38 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 def no_c(my_string):
-	res_str = ''.join(i for i in my_string if i not in 'cC')
-	return(res_str)
+    return(''.join(i for i in my_string if i not in 'cC'))
+    
