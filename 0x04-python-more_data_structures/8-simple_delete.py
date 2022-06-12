6722555739e8b9c17a00bab219e6b534a3b88c68
Mercy Kanyi <99387423+Mercy-Kanyi@users.noreply.github.com>
2022-06-12 21:20:19 +0300

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+# a function that deletes a key in a dictionary
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary.get(key) is not None:
+    if key:
         del a_dictionary[key]
-    return (a_dictionary)
+        return(a_dictionary)
+    return(a_dictionary)
