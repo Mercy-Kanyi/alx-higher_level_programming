bc34d9b6289feb392167ff405ba22b922f28040b
Mercy Kanyi <99387423+Mercy-Kanyi@users.noreply.github.com>
2022-06-20 23:24:21 +0300

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,9 +3,9 @@
 # a function that divides 2 integers and prints the result
 def safe_print_division(a, b):
     try:
-        var = a / b
+        result = a / b
     except ZeroDivisionError:
-        var = None
+        result = None
     finally:
-        print("Inside result: {}".format(var))
-    return var
+        print("Insise result: {}".format(result))
+    return result
