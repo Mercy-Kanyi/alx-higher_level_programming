def35a5ab267f527e871153b0541de4f92306622
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-06-20 19:07:57 +0000

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 # a function that prints an integer with "{:d}".format()
 def safe_print_integer(value):
     try:
-        print("{:d}".format(int(value)))
+        print("{:d}".format(value))
         return True
     except ValueError as ex:
         return False
