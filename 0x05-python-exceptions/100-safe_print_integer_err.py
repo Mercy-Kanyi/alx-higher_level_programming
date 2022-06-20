09f4a0023421a03460e7ecd7e49a871c10d8d4ec
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-06-20 21:42:43 +0000

    second

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# function that prints an integer
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as i:
+        print("Exception: {}".format(i), file=sys.stderr)
+        return False
+
+    return value
